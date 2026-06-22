819d5ca2af8393f002b65fe156e7cc464e993bc3
BilgiPasa <bilgicaglarbozkurt@gmail.com>
2026-06-22 22:01:37 +0300

    Update 2 comments in player script

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -3,13 +3,13 @@ extends RigidBody3D
 
 var bumping: bool = false
 var bumped_body: Node3D = null
-var move_vector: Vector2 # X is X, Y is Z.
+var move_vector: Vector2 # X is X, Y is -Z.
 
 func _ready() -> void:
 	process_mode = Node.PROCESS_MODE_PAUSABLE
 
 func _process(_delta: float) -> void:
-	# move_vector's X is X in 3D, Y is Z in 3D. Also, it is normalized.
+	# move_vector's X is X in 3D, Y is -Z in 3D. Also, it is normalized.
 	move_vector = Vector2(Input.get_axis("move_left", "move_right"), Input.get_axis("move_back", "move_forward")).normalized()
 
 func _on_bump_area_body_entered(body: Node3D) -> void:
