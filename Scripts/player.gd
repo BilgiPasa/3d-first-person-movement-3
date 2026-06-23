3d155361ec93372dd4130838d7f2f1f347b68ff4
BilgiPasa <bilgicaglarbozkurt@gmail.com>
2026-06-23 10:40:58 +0300

    Update player script, Update main scene, Update player_and_camera scene and its script, Update the gravity amount

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,16 +1,83 @@
 class_name Player
 extends RigidBody3D
 
-var bumping: bool = false
-var bumped_body: Node3D = null
+# Note: I changed the default gravity to 30 m/s^2 in Project Settings
+# Note2: I have set the layers and the masks in the editor.
+
+# Movement
+const NORMAL_SPEED: int = 9
+const RUN_SPEED: int = 12
+var run_input: bool
 var move_vector: Vector2 # X is X, Y is -Z.
+
+# Crouch
+var crouch_input: bool
+
+# Jump
+var jump_input: bool
+
+# Collision and Area Detection
+var touching: bool = false
+var grounded: bool = false
+var bumping: bool = false
+var touched_body: Node3D = null
+var grounded_body: Node3D = null
+var bumped_body: Node3D = null
+
+# Player Sizes
+const PLAYER_HEIGHT: float = 2.0
+const CROUCH_HEIGHT: float = 1.5
+const PLAYER_RADIUS: float = 0.5
+
+# Some Player Components
+@export var player_mesh: CapsuleMesh
+@export var player_coll_shape: CapsuleShape3D
+@export var camera_position: Node3D
+@export var grounded_area: Area3D
+@export var bump_area: Area3D
 
 func _ready() -> void:
 	process_mode = Node.PROCESS_MODE_PAUSABLE
+	can_sleep = false
+	continuous_cd = true
+	contact_monitor = true
+	max_contacts_reported = 10
+	player_mesh.height = PLAYER_HEIGHT
+	player_mesh.radius = PLAYER_RADIUS
+	player_coll_shape.height = PLAYER_HEIGHT
+	player_coll_shape.radius = PLAYER_RADIUS
+	camera_position.position.y = (PLAYER_HEIGHT / 2) - 0.25
+	grounded_area.position.y = -(PLAYER_HEIGHT / 2)
+	bump_area.position.z = -(PLAYER_RADIUS / 2)
 
 func _process(_delta: float) -> void:
 	# move_vector's X is X in 3D, Y is -Z in 3D. Also, it is normalized.
 	move_vector = Vector2(Input.get_axis("move_left", "move_right"), Input.get_axis("move_back", "move_forward")).normalized()
+
+	# Other inputs
+	run_input = Input.is_action_pressed("run")
+	crouch_input = Input.is_action_pressed("crouch")
+	jump_input = Input.is_action_pressed("jump")
+
+func _on_body_entered(body: Node) -> void:
+	if body == null:
+		touched_body = body
+		touching = true
+
+func _on_body_exited(body: Node) -> void:
+	if body == touched_body:
+		touched_body = null
+		touching = false
+
+func _on_grounded_area_body_entered(body: Node3D) -> void:
+	if body == null:
+		grounded_body = body
+		grounded = true
+
+func _on_grounded_area_body_exited(body: Node3D) -> void:
+	if body == grounded_body:
+		grounded_body = null
+		grounded = false
 
 func _on_bump_area_body_entered(body: Node3D) -> void:
 	if body == null:
--- Scripts/player_and_camera.gd
+++ Scripts/player_and_camera.gd
@@ -4,9 +4,16 @@ extends Node3D
 @export var player: Player
 @export var camera: Camera3D
 @export var camera_position: Node3D
+var mouse_sensitivity: int
+var x_rotation: int
+var y_rotation: int
+var camera_zoom_input: bool
 
 func _ready() -> void:
 	process_mode = Node.PROCESS_MODE_PAUSABLE
+
+func _process(delta: float) -> void:
+	camera_zoom_input = Input.is_action_pressed("camera_zoom")
 
 func get_player_speed() -> float:
 	return sqrt(pow(player.linear_velocity.x, 2) + pow(player.linear_velocity.z, 2))
