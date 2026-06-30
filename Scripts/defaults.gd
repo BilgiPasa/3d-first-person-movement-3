70a60ab6c9acfca08d4f3df9e3f0e6b183bd882b
BilgiPasa <bilgicaglarbozkurt@gmail.com>
2026-06-30 09:19:09 +0300

    Update Environment scene, Increase gravity to 40, Change jump_force to 13, Update to version 0.6

--- Scripts/defaults.gd
+++ Scripts/defaults.gd
@@ -1,7 +1,7 @@
 extends Node
 
 var normal_speed: int = 9
-var jump_force: int = 12
+var jump_force: int = 13
 var mouse_sensitivity: int = 100
 var normal_fov: int = 90
 var dynamic_fov: bool = true
